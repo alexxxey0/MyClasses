ba7b3dc4134a693406478ae7192c566323530c1b
alexxxey <aleksejs.gorlovics@gmail.com>
2025-09-17 13:09:09 +0300

    Creating the assignment and saving it to the database.

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Assignment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 
 class AssignmentController extends Controller {
     public function create_assignment(Request $request) {
-        dd($request->all());
-        // TODO: validate and add assignment to the database
+        $form_fields = $request->validate([
+            'name' => ['required', 'max:100'],
+            'description' => ['nullable', 'max:500'],
+            'type' => ['required'],
+            'deadline' => ['required', 'date']
+        ]);
+        $form_fields['class_id'] = $request->class_id;
+
+        Assignment::create($form_fields);
+
+        return to_route('assignments')->with('flash_message', 'Assignment created successfully!');
     }
 }
